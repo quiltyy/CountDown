5fae597b167c10ee89420aca156e5158f0a27f75
quiltyy <achamberlain008@gmail.com>
2023-05-26 09:49:03 -0700

    Initial Commit

---
+++ CountDown/CountDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownApp.swift
+//  CountDown
+//
+//  Created by Andrew Chamberlain on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CountDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
